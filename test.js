066cd18dcff3682c309529dbf432179e49f85c52
mansi-hp <mansiagnihotri037@gmail.com>
2026-09-24 14:07:55 +0530

    Add automated tests in test.js

---
+++ test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+console.log("Running automated tests...");
+// Simple passing test assert
+assert.strictEqual(1 + 1, 2);
+console.log("All tests passed successfully!");
